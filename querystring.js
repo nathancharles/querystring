2cc0a9871821a764db091b8dc8e67f6939dd42cc
Nathan <nathancookster@gmail.com>
2014-01-02 22:42:29 -0600

    flesh-out function to parse query string and call itself

--- querystring.js
+++ querystring.js
@@ -1,5 +1,15 @@
 (function(window) {
-	var parameters = {};
+	var _queryString = {};
+
+	(function(){
+		var query = window.location.href.split('?')[1],
+			queryArray = query.split('&'),
+			paramParts;
+		for(var i = 0; i < queryArray.length; i++) {
+			paramParts = queryArray[i].split('=');
+			_queryString[paramParts[0]] = (paramParts[1]) ? paramParts[1] : '';
+		}
+	}());
 
 	var querystring = function querystring() {
 
