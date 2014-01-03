694923aa8283fd898079adfc7b3b2423590576a8
Nathan <nathancookster@gmail.com>
2014-01-02 22:53:41 -0600

    pass the current page url into the iffe

--- querystring.js
+++ querystring.js
@@ -1,15 +1,16 @@
 (function(window) {
 	var _queryString = {};
 
-	(function(){
-		var query = window.location.href.split('?')[1],
+	(function(currentUrl){
+		var query = currentUrl.split('?')[1],
 			queryArray = query.split('&'),
 			paramParts;
+
 		for(var i = 0; i < queryArray.length; i++) {
 			paramParts = queryArray[i].split('=');
 			_queryString[paramParts[0]] = (paramParts[1]) ? paramParts[1] : '';
 		}
-	}());
+	}(window.location.href));
 
 	var querystring = function querystring(name) {
 		if(arguments.length > 0) {
